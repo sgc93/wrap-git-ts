1967135c07267b3404f8b6536ffd0fdb11c07eac
sgc93 <smachewgedefaw@gmail.com>
2025-11-09 21:04:56 +0300

    export types

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import { commitSummarizar } from "./core/commitSummarizer.js";
 import { repoSummarizer } from "./core/repoSummarizer.js";
 import { lngSummarizer } from "./core/lngSummarizer.js";
 
+import { CommitType } from "./types/CommitType.js";
+import { ProfileType } from "./types/ProfileType.js";
+import { LngType } from "./types/Lngtype.js";
+import { RepoType } from "./types/RepoType.js";
+
 export { getUserProfile };
 export { commitSummarizar };
 export { repoSummarizer };
 export { lngSummarizer };
+
+export type { CommitType, ProfileType, LngType, RepoType };
 
 export default {
   getUserProfile,
