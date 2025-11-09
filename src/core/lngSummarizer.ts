092df73bf4361a0d7a30ba45e07b72b7557e92c1
sgc93 <smachewgedefaw@gmail.com>
2025-11-09 20:52:14 +0300

    update: add repo count for each language

--- src/core/lngSummarizer.ts
+++ src/core/lngSummarizer.ts
@@ -1,34 +1,13 @@
 import { getAllRepos, getRepoLngs } from "../api/repos.js";
+import { LngType } from "../types/Lngtype.js";
 import { RepoType } from "../types/RepoType.js";
+import { throttleAll } from "../utils/throttleAll.js";
 
-type LngCoverageType = {
-  lngName: string;
-  coverage: number;
-};
-
-const getTopLng = (lngCoverage: LngCoverageType[]) => {
-  let topCoverage = 0;
-  let toplngName = "";
-
-  for (let { lngName, coverage } of lngCoverage) {
-    if (topCoverage < coverage) {
-      toplngName = lngName;
-      topCoverage = coverage;
-    }
-  }
-
-  return { lngName: toplngName, lngCoverage: topCoverage };
-};
-
-const getTopLanguages = (lngCoverage: LngCoverageType[]) => {
-  const sortedLngCoverage = lngCoverage.sort((a, b) => b.coverage - a.coverage);
-
-  const topFive =
-    sortedLngCoverage.length > 5
-      ? sortedLngCoverage.slice(0, 5)
-      : sortedLngCoverage;
-
-  return topFive;
+const getTopLanguages = (lngCoverage: LngType[]) => {
+  const sortedLngCoverage = [...lngCoverage].sort(
+    (a, b) => b.coverage - a.coverage
+  );
+  return sortedLngCoverage.slice(0, 5);
 };
 
 export const lngSummarizer = async (username: string, token?: string) => {
@@ -37,25 +16,40 @@ export const lngSummarizer = async (username: string, token?: string) => {
     .filter((repo) => repo.owner === username)
     .map((repo) => repo.url);
 
-  const lngStats: Record<string, number> = {};
+  const lngStats: LngType[] = [];
   let totalCodeSize = 0;
 
-  for (const url of urls) {
-    const languages = await getRepoLngs(url, token);
+  const languages = await throttleAll(urls, 10, async (url) => {
+    return await getRepoLngs(url, token);
+  });
 
-    for (const [language, bytes] of Object.entries(languages)) {
-      if (!lngStats[language]) {
-        lngStats[language] = 0;
+  // calc coverage of each language
+  for (const language of languages) {
+    if (!language) continue;
+
+    for (const [lng, bytes] of Object.entries(language)) {
+      const existing = lngStats.find((stat) => stat.lngName === lng);
+
+      if (existing) {
+        existing.coverage += bytes as number;
+      } else {
+        lngStats.push({
+          lngName: lng,
+          coverage: bytes as number,
+          repos: 0
+        });
       }
-
-      lngStats[language] += bytes as number;
       totalCodeSize += bytes as number;
     }
   }
 
-  const lngs = Object.entries(lngStats);
-  const totalLngs = lngs.length;
-  const lngCoverage: LngCoverageType[] = [];
+  // cal no of repos for each lng
+  for (const repo of allRepos) {
+    const existing = lngStats.find((stat) => stat.lngName === repo.language);
+    if (existing) {
+      existing.repos += 1;
+    }
+  }
 
   /*
    *  Estimated LOC = Code size in bytes / Average bytes per line
@@ -65,18 +59,12 @@ export const lngSummarizer = async (username: string, token?: string) => {
 
   const loc = Math.ceil(totalCodeSize / 70);
 
-  for (const [lng, bytes] of lngs) {
-    lngCoverage.push({ lngName: lng, coverage: (bytes / totalCodeSize) * 100 });
-  }
-
-  const topLanguage = getTopLng(lngCoverage);
-  const topThreeLanguages = getTopLanguages(lngCoverage);
+  const top5Lngs = getTopLanguages(lngStats);
 
   return {
-    totalLineOfCode: loc,
-    totalLngs,
-    topThreeLanguages,
-    lngCoverage,
-    topLanguage,
+    totLineOfCode: loc,
+    totalLngs: lngStats.length,
+    top5Lngs,
+    allLngs: lngStats
   };
 };
