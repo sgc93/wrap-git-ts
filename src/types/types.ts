6cbd1badca97a42bff43bd45c63c7678fffe4ba8
sgc93 <smachewgedefaw@gmail.com>
2025-12-19 01:14:49 +0300

    update: add commit response type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,14 +1,22 @@
 export type GitHubProfile = {
-    name: string,
-    login: string,
-    bio: string,
-    avatarUrl: string,
-    company: string | null,
-    location: string | null,
-    email: string | null,
-    followers: { totalCount: number },
-    following: { totalCount: number },
-    starredRepositories: { totalCount: number },
-    repositories: { totalCount: number },
-    createdAt: string,
-  }
+  name: string;
+  login: string;
+  bio: string;
+  avatarUrl: string;
+  company: string | null;
+  location: string | null;
+  email: string | null;
+  followers: { totalCount: number };
+  following: { totalCount: number };
+  starredRepositories: { totalCount: number };
+  repositories: { totalCount: number };
+  createdAt: string;
+};
+
+export type GitHubCommit = {
+  totalCommits: number;
+  commitsPerYear: {
+    year: number;
+    count: number;
+  }[];
+};
