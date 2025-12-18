f68cc7e3e4c4323b46a02dcb9542e401d42de6f4
sgc93 <smachewgedefaw@gmail.com>
2025-12-19 00:19:22 +0300

    update: move profile query to separate file

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,60 +5,23 @@ export async function githubGraphQL<T>(
   variables: Record<string, any> = {},
   token?: string
 ): Promise<T> {
-  const headers = token
-    ? {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${token}`
-      }
-    : {
-        "Content-Type": "application/json"
-      };
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json"
+  };
+
+  if (token) headers.Autorization = `bearer ${token}`;
 
   const res = await fetch(GITHUB_GRAPHQL_URL, {
     method: "POST",
     headers: headers as HeadersInit,
     body: JSON.stringify({ query, variables })
   });
-  
+
   const json = await res.json();
-  console.log(json)
-  if (json.errors){
-   const error =  JSON.stringify(json.errors, null, 2);
-   console.log(json.errors)
+  console.log(json);
+  if (json.errors) {
+    const error = JSON.stringify(json.errors, null, 2);
+    console.log(json.errors);
   }
   return json.data;
-}
-
-// queries
-const userDataQuery = `
-query GetProfile($login: String!) {
-  user(login: $login) {
-    id
-    name
-    login
-    bio
-    avatarUrl
-    company
-    location
-    email
-    followers {
-      totalCount
-    }
-    following {
-      totalCount
-    }
-    createdAt
-    starredRepositories {
-      totalCount
-    }
-    repositories {
-      totalCount
-    }
-  }
-}`;
-
-export const getUserProfile = async (usernae: string, token?: string) => {
-  const profileData = await githubGraphQL(userDataQuery, { login: usernae }, token);
-
-  return profileData;
 }
