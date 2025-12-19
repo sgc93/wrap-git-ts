978605de5cf3ff2d314637eda4ca4b4a3a00de8a
sgc93 <smachewgedefaw@gmail.com>
2025-12-19 22:00:03 +0300

    update: export all new methods

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
 import { getGitHubUser } from "./core/profile.js";
 import { getGitHubCommits } from "./core/commits.js";
+import { getGitHubLanguages } from "./core/languages.js";
+import { getGitHubRepos, getGitHubPinnedRepos } from "./core/repositories.js";
 import {
   getGitHubContributions,
   getGitHubContributionTypesPerYear
 } from "./core/contributions.js";
 
-import { GitHubProfile, GitHubCommit } from "./types/types.js";
+import {
+  GitHubProfile,
+  GitHubCommit,
+  UserLangStat,
+  GitHubRepo
+} from "./types/types.js";
 
 export {
   getGitHubUser,
   getGitHubCommits,
   getGitHubContributions,
-  getGitHubContributionTypesPerYear
+  getGitHubContributionTypesPerYear,
+  getGitHubLanguages,
+  getGitHubRepos,
+  getGitHubPinnedRepos
 };
 
-export type { GitHubProfile, GitHubCommit };
+export type { GitHubProfile, GitHubCommit, UserLangStat, GitHubRepo };
 
 export default {
   getGitHubUser,
   getGitHubCommits,
-  getGitHubContributions
+  getGitHubContributions,
+  getGitHubContributionTypesPerYear,
+  getGitHubLanguages,
+  getGitHubRepos,
+  getGitHubPinnedRepos
 };
