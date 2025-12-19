eb610696661a05dc994fa73ce1631ac7fa0b0659
sgc93 <smachewgedefaw@gmail.com>
2025-12-19 22:39:40 +0300

    update: export GitHubContribution type

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ import { getGitHubLanguages } from "./core/languages.js";
 import { getGitHubRepos, getGitHubPinnedRepos } from "./core/repositories.js";
 import {
   getGitHubContributions,
-  getGitHubContributionTypesPerYear
+  getGitHubContributionTypesPerYear,
+  getGitHubYearlyContributions
 } from "./core/contributions.js";
 
 import {
   GitHubProfile,
   GitHubCommit,
   UserLangStat,
-  GitHubRepo
+  GitHubRepo,
+  GitHubContribution
 } from "./types/types.js";
 
 export {
@@ -19,18 +21,26 @@ export {
   getGitHubCommits,
   getGitHubContributions,
   getGitHubContributionTypesPerYear,
+  getGitHubYearlyContributions,
   getGitHubLanguages,
   getGitHubRepos,
   getGitHubPinnedRepos
 };
 
-export type { GitHubProfile, GitHubCommit, UserLangStat, GitHubRepo };
+export type {
+  GitHubProfile,
+  GitHubCommit,
+  UserLangStat,
+  GitHubRepo,
+  GitHubContribution
+};
 
 export default {
   getGitHubUser,
   getGitHubCommits,
   getGitHubContributions,
   getGitHubContributionTypesPerYear,
+  getGitHubYearlyContributions,
   getGitHubLanguages,
   getGitHubRepos,
   getGitHubPinnedRepos
