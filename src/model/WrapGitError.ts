41ad1117197b94e595513f8e4e6393958b72fb48
sgc93 <smachewgedefaw@gmail.com>
2025-12-21 16:47:54 +0300

    fix: set protype of WrapGitError

--- src/model/WrapGitError.ts
+++ src/model/WrapGitError.ts
@@ -1,12 +1,15 @@
-class GitWrapperError extends Error {
+class WrapGitError extends Error {
   status: string;
   details: string;
-  
+
   constructor(status: string, message: string, details: string) {
     super(message);
     this.status = status;
     this.details = details;
+    this.name = "WrapGitError";
+
+    Object.setPrototypeOf(this, WrapGitError.prototype);
   }
 }
 
-export default GitWrapperError;
+export default WrapGitError;
